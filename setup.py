456079b50c3a415a11a92565706474729f29f692
Ken <kenjyco@gmail.com>
2022-04-04 11:44:34 -0500

    Initial commit

---
+++ setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup, find_packages
+
+
+with open('README.rst', 'r') as fp:
+    long_description = fp.read()
+
+with open('requirements.txt', 'r') as fp:
+    requirements = fp.read().splitlines()
+
+setup(
+    name='testing-helper',
+    version='0.0.1',
+    description='Helpers for testing with pytest',
+    long_description=long_description,
+    author='Ken',
+    author_email='kenjyco@gmail.com',
+    license='MIT',
+    url='https://github.com/kenjyco/testing-helper',
+    download_url='https://github.com/kenjyco/testing-helper/tarball/v0.0.1',
+    packages=find_packages(),
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest'],
+    install_requires=requirements,
+    include_package_data=True,
+    package_dir={'': '.'},
+    package_data={
+        '': ['*.ini'],
+    },
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Software Development :: Libraries',
+        'Intended Audience :: Developers',
+    ],
+    keywords=['testing', 'pytest', 'test matrix', 'docker', 'pyenv', 'helper']
+)
